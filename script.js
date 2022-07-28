12d23bc4dff103733783663a4c71709f5ae0f21f
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2022-07-28 11:36:40 -0400

    conversor

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch('http://economia.awesomeapi.com.br/json/last/USD-BRL').then(response=>{
+    return response.json()
+}).then(bd => {
+
+    const dol = (bd.USDBRL.bid);
+    const real = document.getElementById('real');
+    const btn = document.getElementsByTagName('button')[0]
+    
+    function converter(m1){
+        return m1 * dol
+    }
+    
+    btn.addEventListener('click', () => {
+    
+        const res = document.getElementsByTagName('p')[0];
+        let con =  converter(Number(real.value));
+        res.innerHTML = '$ ' + con.toFixed(2    );
+    })
+
+})
+
